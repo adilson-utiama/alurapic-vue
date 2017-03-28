1b81d99e6dc80c3eb4305ab1b5fb03fa0b4c694a
Adilson <asu2u@yahoo.com.br>
2017-03-28 00:02:20 -0300

    Commit Inicial

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <img :src="foto.url" v-bind:alt="foto.titulo"><!-- Podemos usar o atalho ':' ou 'v-bind:' -->
+  </div>
+</template>
+
+<script>
+export default {
+
+   data () {
+
+    return {
+      titulo: 'Alurapic Vue',
+      foto: {
+          url: 'http://www.farejadordecaes.com.br/wp-content/uploads/o-que-saber-antes-de-comprar-cachorro-01.png',
+          titulo: 'cachorro'
+      }
+
+    }
+
+  }
+}
+</script>
+
+<style>
+
+</style>
