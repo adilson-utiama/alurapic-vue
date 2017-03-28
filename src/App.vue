24753ce07582911ee7ba758d431b7deb81b273e1
Adilson <asu2u@yahoo.com.br>
2017-03-28 00:19:26 -0300

    Adequando o template gerado

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h1>{{ titulo }}</h1>
-    <img :src="foto.url" v-bind:alt="foto.titulo"><!-- Podemos usar o atalho ':' ou 'v-bind:' -->
+    <ul>
+      <li v-for="foto of fotos"><!-- Diretiva de laco 'v-for' -->
+        <img :src="foto.url" v-bind:alt="foto.titulo"><!-- Podemos usar o atalho ':' ou 'v-bind:' -->
+      </li>
+    </ul>
+    
   </div>
 </template>
 
@@ -12,10 +17,16 @@ export default {
 
     return {
       titulo: 'Alurapic Vue',
-      foto: {
+      fotos: [
+        {
           url: 'http://www.farejadordecaes.com.br/wp-content/uploads/o-que-saber-antes-de-comprar-cachorro-01.png',
           titulo: 'cachorro'
-      }
+        },
+        {
+          url: 'http://res.cloudinary.com/db79cecgq/image/upload/c_crop,h_811,w_1437,x_0,y_44/c_fill/v1407165291/os-10-animais-mais-fofos-do-mundo-03.jpg',
+          titulo: 'cachorro 2'
+        }
+      ]
 
     }
 
