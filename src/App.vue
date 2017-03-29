36e4121744a78d3d91267e82c5e926bbf2fbd843
Adilson <asu2u@yahoo.com.br>
2017-03-28 23:03:13 -0300

    Integrando API de terceiros com vue-resource

--- src/App.vue
+++ src/App.vue
@@ -18,19 +18,19 @@ export default {
     return {
       titulo: 'Alurapic Vue',
       fotos: [
-        {
-          url: 'http://www.farejadordecaes.com.br/wp-content/uploads/o-que-saber-antes-de-comprar-cachorro-01.png',
-          titulo: 'cachorro'
-        },
-        {
-          url: 'http://res.cloudinary.com/db79cecgq/image/upload/c_crop,h_811,w_1437,x_0,y_44/c_fill/v1407165291/os-10-animais-mais-fofos-do-mundo-03.jpg',
-          titulo: 'cachorro 2'
-        }
+        
       ]
-
     }
+  },
+
+  created() {
+
+    this.$http.get('http://localhost:3000/v1/fotos')
+        .then(res => res.json())
+        .then(fotos => this.fotos = fotos, err => console.log(err));
 
   }
+  
 }
 </script>
 
