992967f201b6058183205f63f2a3c3e194254a1a
Adilson <asu2u@yahoo.com.br>
2017-03-29 23:34:58 -0300

    Adicionando comportamento ao painel

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -1,10 +1,11 @@
 <template>
 
 <div class="painel">
-    <h2 class="painel-titulo">{{ titulo }}</h2>
-    <slot class="painel-conteudo">
-        <!-- Conteudo a ser recebido aqui. -->
-    </slot>
+    <h2 class="painel-titulo" v-on:dblclick="visivel = !visivel">{{ titulo }}</h2>
+    <div class="painel-conteudo" v-show="visivel">
+        <slot ><!-- Conteudo a ser recebido aqui. --></slot>
+    </div>
+    
 </div>
        
        
@@ -14,7 +15,13 @@
 
 export default {
 
-    props: ['titulo']
+    props: ['titulo'],
+
+    data() {
+        return {
+            visivel: true
+        }
+    }
     
 }
 
